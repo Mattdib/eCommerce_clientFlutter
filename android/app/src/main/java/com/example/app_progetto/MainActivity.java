c6f2ec4e027ad56a9d83540598d586fb4232709e
Mattdib <62659315+Mattdib@users.noreply.github.com>
2024-07-13 00:31:19 +0200

    Android file package changes.

--- android/app/src/main/java/com/example/app_progetto/MainActivity.java
+++ android/app/src/main/java/com/example/app_progetto/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.app_progetto;
 
-import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity { //extends FlutterActivity
 }
